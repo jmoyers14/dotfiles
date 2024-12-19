a2bb667af17be4a7eac9242b6782af92e81a26a8
Jeremy Moyers <jeremy@jeremymoyers.com>
2024-12-18 20:07:41 -0800

    fish config

---
+++ fish/conf.d/fnm.fish
@@ -0,0 +1,4 @@
+#fnm env --use-on-cd | source
+# fnm
+set PATH "/Users/jmoyers/Library/Application Support/fnm" $PATH
+fnm env | source
---
+++ fish/config.fish
@@ -0,0 +1,45 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+fish_add_path /Users/jmoyers/.cargo/bin
+
+alias erc="vim ~/.config/fish/config.fish"
+alias proj="cd ~/Projects"
+alias ptmo="cd ~/Projects/trova-mobile"
+alias ptmo="cd ~/Projects/trova-mobile-api-services"
+alias pdt="cd ~/Projects/dynamic-trips"
+alias ptb="cd ~/Projects/trova-buddy"
+alias optm="cd ~/Projects/trova-maven"
+alias ptc="cd ~/Projects/trova-core"
+alias ptma="cd ~/Projects/trova-micro-apps"
+alias ptd="cd ~/Projects/trova-devops"
+alias ptbook="cd ~/Projects/trova-booking"
+alias ptbooka="cd ~/Projects/trova-booking-api-services"
+alias ptcomp="cd ~/Projects/trova-components"
+alias ptm="cd ~/Projects/trova-maven"
+alias ptn="cd ~/Projects/trova-mobile"
+alias optm="code ~/Projects/trova-maven"
+alias dem="cd ~/Demos"
+alias doc="cd ~/Documents"
+alias proj="cd ~/Projects"
+alias mplay="cd ~/Documents/MongoPlaygrounds"
+alias mapi="cd ~/Documents/trova-api"
+alias tlogout="rm /var/tmp/com.cooltools.remit-trip.cookie"
+alias list-node="ls /usr/local/Cellar | grep node"
+alias node16="brew link --overwrite node@16"
+
+
+if test -f ~/secrets.fish
+    source ~/secrets.fish
+end
+
+set -gx ANDROID_HOME $HOME/Library/Android/sdk
+set -gx PATH $PATH:$ANDROID_HOME/emulator
+set -gx PATH $PATH:$ANDROID_HOME/platform-tools
+
+set PATH $PATH /usr/local/bin /usr/local/go/bin /Library/TeX/texbin/pdflatex
+starship init fish | source
+status --is-interactive; and rbenv init - fish | source
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/jmoyers/google-cloud-sdk/path.fish.inc' ]; . '/Users/jmoyers/google-cloud-sdk/path.fish.inc'; end
