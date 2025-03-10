b342443108c343809fd1cd01d5805636ffb506f9
Jeremy Moyers <jeremymoyers@0134Admins-Air.localdomain>
2025-03-10 08:28:38 -0700

    remove naviation aliases

--- fish/config.fish
+++ fish/config.fish
@@ -7,30 +7,7 @@ fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
 
 alias erc="vim ~/.config/fish/config.fish"
-alias proj="cd ~/Projects"
-alias ptmo="cd ~/Projects/trova-mobile"
-alias ptmo="cd ~/Projects/trova-mobile-api-services"
-alias pdt="cd ~/Projects/dynamic-trips"
-alias ptb="cd ~/Projects/trova-buddy"
-alias optm="cd ~/Projects/trova-maven"
-alias ptc="cd ~/Projects/trova-core"
-alias ptma="cd ~/Projects/trova-micro-apps"
-alias ptd="cd ~/Projects/trova-devops"
-alias ptbook="cd ~/Projects/trova-booking"
-alias ptbooka="cd ~/Projects/trova-booking-api-services"
-alias ptcomp="cd ~/Projects/trova-components"
-alias ptm="cd ~/Projects/trova-maven"
-alias ptn="cd ~/Projects/trova-mobile"
-alias optm="code ~/Projects/trova-maven"
-alias dem="cd ~/Demos"
-alias doc="cd ~/Documents"
-alias proj="cd ~/Projects"
-alias mplay="cd ~/Documents/MongoPlaygrounds"
-alias mapi="cd ~/Documents/trova-api"
 alias tlogout="rm /var/tmp/com.cooltools.remit-trip.cookie"
-alias list-node="ls /usr/local/Cellar | grep node"
-alias node16="brew link --overwrite node@16"
-
 
 set current_dir (dirname (status filename))
 if test -f $current_dir/secrets.fish
