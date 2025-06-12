cd876ede43c7066ad20b86a10def2cc0bc20e040
Jeremy Moyers <>
2025-06-12 08:34:50 -0700

    update avante config

--- nvim/lua/jeremymoyers/plugins/avante.lua
+++ nvim/lua/jeremymoyers/plugins/avante.lua
@@ -36,11 +36,15 @@ return {
     opts = {
         provider = "claude",
         auto_suggestions_provider = "claude",
-        claude = {
-            endpoint = "https://api.anthropic.com",
-            model = "claude-3-5-sonnet-20241022",
-            temperature = 0,
-            max_tokens = 4096,
+        providers = {
+            claude = {
+                endpoint = "https://api.anthropic.com",
+                model = "claude-3-5-sonnet-20241022",
+                extra_request_body = {
+                    temperature = 0,
+                    max_tokens = 4096,
+                }
+            },
         },
         dual_boost = {
             enabled = false,
