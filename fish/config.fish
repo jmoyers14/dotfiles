3f799823fe6b9afb1dbf60bbaa395b213746401d
Jeremy Moyers <jeremy@jeremymoyers.com>
2026-05-20 14:34:54 -0700

    setup sessionizer script for git worktrees; fix some broken paths; nvim packge.lock

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-fish_add_path /Users/jmoyers/.cargo/bin
-fish_add_path /Users/jmoyers/Personal/dotfiles/bin/.local/scripts
+fish_add_path ~/.cargo/bin
+fish_add_path ~/Personal/dotfiles/bin/.local/scripts
 fish_add_path /opt/homebrew/opt/ruby/bin
 fish_add_path /opt/homebrew/lib/ruby/gems/3.4.0/bin
 fish_add_path /opt/homebrew/bin
